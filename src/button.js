7c744f155d992b4fe8f58c9dead692f4e0f1e304
Marco <marco.mannucci@hey.com>
2021-10-16 08:53:09 +0200

    feat: label attribute

--- src/button.js
+++ src/button.js
@@ -40,7 +40,23 @@ class Button extends HTMLElement {
 		super();
 		this._shadowRoot = this.attachShadow({ mode: 'open' });
 		this._shadowRoot.appendChild(template.content.cloneNode(true));
+
+		this.$button = this._shadowRoot.querySelector('button');
 	}
+
+	static get observedAttributes() {
+		return ['label']
+	}
+
+	attributeChangedCallback(name, oldVal, newVal) {
+		this[name] = newVal;
+
+		this.render();
+	}
+
+    render() {
+        this.$button.innerHTML = this.label;
+    }
 }
 
 window.customElements.define('my-button', Button);
